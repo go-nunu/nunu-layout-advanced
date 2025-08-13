8a641c43e3c4163e2d2fd1ee5e43e98045877042
chris <5303221@gmail.com>
2025-08-13 14:19:30 +0800

    fix: Remove the redundant wrapping parentheses

--- internal/router/user.go
+++ internal/router/user.go
@@ -9,23 +9,21 @@ func InitUserRouter(
 	r *gin.RouterGroup,
 	deps RouterDeps,
 ) {
+	// No route group has permission
+	noAuthRouter := r.Group("/")
 	{
-		// No route group has permission
-		noAuthRouter := r.Group("/")
-		{
-			noAuthRouter.POST("/register", deps.UserHandler.Register)
-			noAuthRouter.POST("/login", deps.UserHandler.Login)
-		}
-		// Non-strict permission routing group
-		noStrictAuthRouter := r.Group("/").Use(middleware.NoStrictAuth(deps.JWT, deps.Logger))
-		{
-			noStrictAuthRouter.GET("/user", deps.UserHandler.GetProfile)
-		}
+		noAuthRouter.POST("/register", deps.UserHandler.Register)
+		noAuthRouter.POST("/login", deps.UserHandler.Login)
+	}
+	// Non-strict permission routing group
+	noStrictAuthRouter := r.Group("/").Use(middleware.NoStrictAuth(deps.JWT, deps.Logger))
+	{
+		noStrictAuthRouter.GET("/user", deps.UserHandler.GetProfile)
+	}
 
-		// Strict permission routing group
-		strictAuthRouter := r.Group("/").Use(middleware.StrictAuth(deps.JWT, deps.Logger))
-		{
-			strictAuthRouter.PUT("/user", deps.UserHandler.UpdateProfile)
-		}
+	// Strict permission routing group
+	strictAuthRouter := r.Group("/").Use(middleware.StrictAuth(deps.JWT, deps.Logger))
+	{
+		strictAuthRouter.PUT("/user", deps.UserHandler.UpdateProfile)
 	}
 }
